2b4f0d07763702c31de599e1af50458f18e17c83
Salikh Magomedov <salikh.magomedov@icloud.com>
2020-11-01 20:07:47 +0300

    Network Manager

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,19 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        Debug.Log("I connected to a server!");
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        Debug.Log($"There are now {numPlayers} players");
+    }
+}
